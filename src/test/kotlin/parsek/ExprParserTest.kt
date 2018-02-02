a320e74065c518d7588891288f966c270b90da78
Christoph Dietze <christoph.dietze@kurzdigital.com>
2018-02-02 23:16:00 +0100

    Make test parsers into Rules to give them names

--- src/test/kotlin/parsek/ExprParserTest.kt
+++ src/test/kotlin/parsek/ExprParserTest.kt
@@ -8,15 +8,15 @@ import kotlin.test.assertEquals
  * https://github.com/lihaoyi/fastparse/blob/master/fastparse/shared/src/test/scala/fastparse/MathTests.scala#L19-L29
  */
 class ExprParserTest {
-    val int: Parser<Int> = (CharIn("+-").opt() * WhileCharIn("0123456789")).capture().map { it.toInt() }
+    val int: Parser<Int> = Rule("int") { (CharIn("+-").opt() * WhileCharIn("0123456789")).capture().map { it.toInt() } }
 
     val parens: Parser<Int> = Rule("parens") { P("(") * addSub * P(")") }
-    val factor: Parser<Int> = int + parens
+    val factor: Parser<Int> = Rule("factor") { int + parens }
 
-    val divMul: Parser<Int> = (factor * ((P("*") + P("/")).capture() * factor).rep()).map(::eval)
-    val addSub: Parser<Int> = (divMul * ((P("+") + P("-")).capture() * divMul).rep()).map(::eval)
+    val divMul: Parser<Int> = Rule("divMul") { (factor * ((P("*") + P("/")).capture() * factor).rep()).map(::eval) }
+    val addSub: Parser<Int> = Rule("addSub") { (divMul * ((P("+") + P("-")).capture() * divMul).rep()).map(::eval) }
 
-    val expr: Parser<Int> = addSub * End
+    val expr: Parser<Int> = Rule("expr") { addSub * End }
 
     private fun eval(it: Pair<Int, List<Pair<String, Int>>>): Int {
         return it.second.fold(it.first, { acc, p ->
