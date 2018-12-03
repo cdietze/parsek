42f51d0cd365490b0dcc27cabbb6afdafaa9d222
Christoph Dietze <christoph.dietze@kurzdigital.com>
2018-12-04 00:11:29 +0100

    Fix build: can not use spaces in JS test function names

--- src/test/kotlin/parsek/MapTests.kt
+++ src/test/kotlin/parsek/MapTests.kt
@@ -9,25 +9,25 @@ class MapTests {
     private val digit = CharIn("0123456789").capture()
 
     @Test
-    fun `map with 2 params should work`() {
+    fun `map_with_2_params_should_work`() {
         val p = (char * char).map { a, b -> a + b }
         assertEquals("ab", p.parse("ab").getOrFail().value)
     }
 
     @Test
-    fun `map with 3 params should work`() {
+    fun `map_with_3_params_should_work`() {
         val p = (char * char * char).map { a, b, c -> a + b + c }
         assertEquals("abc", p.parse("abc").getOrFail().value)
     }
 
     @Test
-    fun `map with 4 params should work`() {
+    fun `map_with_4_params_should_work`() {
         val p = (char * char * char * char).map { a, b, c, d -> a + b + c + d }
         assertEquals("abcd", p.parse("abcd").getOrFail().value)
     }
 
     @Test
-    fun `map with 5 params should work`() {
+    fun `map_with_5_params_should_work`() {
         val p = (char * char * char * char * char).map { a, b, c, d, e -> a + b + c + d + e }
         assertEquals("abcde", p.parse("abcde").getOrFail().value)
     }
